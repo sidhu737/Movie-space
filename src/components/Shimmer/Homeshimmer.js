72506e9390feb4fcb70db478392fe37039b684bf
mahesh-mallareddy <mallareddymahesh0104@gmail.com>
2023-06-03 10:30:59 +0530

    shimmer-style-added

---
+++ src/components/Shimmer/Homeshimmer.js
@@ -0,0 +1,26 @@
+import './shimmer.css'
+const Shimmer = () => {
+    return (
+        <div className='cards'>
+        <div className='cards_shimmer'>
+            <div className="shimmer"></div>
+            <div className='shimmer_header'></div>
+            {/* <div> */}
+                <div className='grid_box'>
+
+                    {
+                        Array(6)
+                            .fill("")
+                            .map((e, index) => (
+                                <div className="shimmer_container" key={index}>
+                                    <section></section>
+                                </div>
+                            ))}
+                </div>
+            {/* </div> */}
+        </div>
+        </div>
+    )
+};
+
+export default Shimmer;
