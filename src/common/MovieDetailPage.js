15c7708b774906021c329d66bcf6b936a78a0dd5
mahesh-mallareddy <mallareddymahesh0104@gmail.com>
2023-06-07 20:02:20 +0530

    tv and movie details updated

--- src/common/MovieDetailPage.js
+++ src/common/MovieDetailPage.js
@@ -7,52 +7,56 @@ import { apiEndpoint } from "./constants";
 const MovieDetailPage = () => {
   const { id } = useParams();
   const [moviedata, setmoviedata] = useState('')
-  
-  const details= async()=> {
+
+  const details =  () => {
     const result = fetchUtil.get(`movie/${id}`)
-    .then((data)=>{
-      setmoviedata(data)
-    }).catch((error)=>{
-      console.log(error.message+"details error")
-    })
-}
+      .then((data) => {
+        setmoviedata(data)
+      }).catch((error) => {
+        console.log(error.message + "details error")
+      })
+  }
   useEffect(() => {
     details()
   }, [])
 
   console.log("MovieDetailPage: " + id)
-  console.log(moviedata.backdrop_path)
-  console.log(moviedata.budget)
+  console.log(moviedata.genres)
+  console.log(moviedata)
+  
   return (
     <div className="detail_page">
       <div className="bg_detail_page" style={{
-        backgroundImage: `url(${apiEndpoint.imageUrl}/kyeqDT2cFhTmwlvtuQY3n2W6Vf3.jpg)`,
+        backgroundImage: `url(${apiEndpoint.imageUrl}${moviedata.backdrop_path})`,
       }}>
         <div className="bg_grad"></div>
         <div className="page_header">
           <div className="header_content">
             <div className="left_content">
-              <img src={`${apiEndpoint.imageUrl}/dniWicB6fa7NvpGbguxWlNPMc5f.jpg`}></img>
+              <img src={`${apiEndpoint.imageUrl}${moviedata.poster_path}`}></img>
             </div>
             <div className="right_content">
-              <div className="original_title">
-                <h1>Guardians of the Galaxy Vol. 3</h1>
-                <p >Once more with feeling.</p>
-              </div>
-              <div className="movie_info">
+            <div className="original_title">
+                <h1>{moviedata.original_title}</h1>
+                <p >{moviedata.tagline}</p>
+            </div>
+            <div className="movie_info">
                 <div className="movie_rating">
-                  <p>rating:4.5</p>
-                  <p>(1817 Votes)</p>
+                    <p>rating:{moviedata.vote_average}</p>
+                    <p>({moviedata.vote_count}votes)</p>
                 </div>
                 <div className="movie_data">
-                  <p>runtime<br /> 150 min</p>
-                  <p> Realse Date<br /> 2023-05-03</p>
+                    <p>runtime <br />{moviedata.runtime} min</p>
+                    <p> Realse Date<br />{moviedata.release_date}</p>
                 </div>
-              </div>
-              <div className="movie_genres">
-                <p>Science Fiction</p>
-                <p>Adventure</p>
-              </div>
+            </div>
+            <div className="movie_genres">
+              {/* {moviedata.genres.map((movie_genres, index) =>{
+                return ( <p key={movie_genres.id}>
+                  {movie_genres.name}</p>) 
+              })
+              } */}
+            </div>
             </div>
           </div>
         </div>
--- src/common/TVDetailPage.js
+++ src/common/TVDetailPage.js
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useState , useEffect } from "react";
 import "./DetailPage.css";
+import { apiEndpoint } from "./constants";
+import fetchUtil from "./fetch-utils";
 import { useParams } from "react-router-dom";
+
 const TVDetailPage = () => {
+  const[TVDetail,setTVDetail] = useState('')
   const { id } = useParams();
+
+  const details =  () => {
+    const result = fetchUtil.get(`tv/${id}`)
+      .then((data) => {
+        setTVDetail(data)
+      }).catch((error) => {
+        console.log(error.message + "details error")
+      })
+  }
+  useEffect(() => {
+    details()
+  }, [])
   console.log("from detail_page", id);
-  return <div className="detail_page">TVDetailPage</div>;
+  return <div className="detail_page">
+    <div className="bg_detail_page" style={{
+      backgroundImage: `url(${apiEndpoint.imageUrl}${TVDetail.backdrop_path})`,
+    }}>
+      <div className="bg_grad"></div>
+      <div className="page_header">
+        <div className="header_content">
+          <div className="left_content">
+            <img src={`${apiEndpoint.imageUrl}${TVDetail.poster_path}`}></img>
+          </div>
+          <div className="right_content">
+            <div className="original_title">
+              <h1>{TVDetail.original_name}</h1>
+              <p >{TVDetail.tagline}</p>
+            </div>
+            <div className="movie_info">
+              <div className="movie_rating">
+                <p>rating:{TVDetail.vote_average}</p>
+                <p>({TVDetail.vote_count}votes)</p>
+              </div>
+              <div className="movie_data">
+                <p>runtime<br />{TVDetail.runtime}</p>
+                <p> Realse Date<br />{TVDetail.release_date}</p>
+              </div>
+            </div>
+            <div className="movie_genres">
+              {/* {TVDetail.genres.map((movie_genres, index) =>{
+                return ( <p key={movie_genres.id}>
+                  {movie_genres.name}</p>) 
+              })
+              } */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
 };
 
 export default TVDetailPage;
